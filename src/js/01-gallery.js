c4a12682c1cefb80119ed762176d69fa72221fac
Wojciech Maroszek <122974346+WojciechMaroszek1@users.noreply.github.com>
2023-06-27 20:50:06 +0200

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,25 +1,5 @@
+// Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from 'simplelightbox';
-import '_../node_modules/simplelightbox/dist/simple-lightbox.min.css';
+// Change code below this line
 
-const imagesGallery = document.querySelector('.gallery');
-const markup = galleryItems
-  .map(
-    galleryItem => `<div class="gallery__item">
-    <a class="gallery__link" href="${galleryItem.original}">
-      <img
-        class="gallery__image"
-        src="${galleryItem.preview}"
-        data-source="${galleryItem.original}"
-        alt="${galleryItem.description}"
-      />
-    </a>
-  </div>`
-  )
-  .join('');
-imagesGallery.insertAdjacentHTML('beforeend', markup);
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
-});
+console.log(galleryItems);
