e508b587e3f44e52fc025bd5b1348954a330b663
Wojciech Maroszek <122974346+WojciechMaroszek1@users.noreply.github.com>
2023-06-28 21:00:57 +0200

    feat: task2, task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
 
+const feedback = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+feedback.addEventListener('input', throttle(getInputData, 500));
+feedback.addEventListener('submit', getEmailData);
+
+function getInputData(event) {
+  const inputData = { email: email.value, message: message.value };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+}
+
+function getEmailData(event) {
+  event.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  feedback.reset();
+  localStorage.clear();
+}
